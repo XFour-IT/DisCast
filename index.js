0638d68bbeaed325554f86a8d8e2bc4f7bb2d793
Benji Lewis <benji@benjisoft.org.uk>
2020-05-12 19:26:24 +0100

    Convert to enviro variables instead of JSON

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const https = require('https');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json')
+const config = process.env;
 
 var playing
 
